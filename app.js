363b224817b0f8b4e6fe77b7e7a3a22d93983b61
Jennifer Rafter <77985983+Jempires@users.noreply.github.com>
2021-10-31 02:21:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 /*
  Authors:
- Your name and student #:
+ Your name and student #:Jennifer Rafter A01071694
  Your Partner's Name and student #:
  (Make sure you also specify on the Google Doc)
 */
 const express = require("express");
-
+let movielistdatabase = [" "];
 let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-let movielist = [" "];
 
 
 
-app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/", (req, res) => res.render("pages/index", { movie: movielistdatabase}));
+  //const namesHtml = names.map(person => `<h2>${person}</h2>`).join("");
+ // res.send(namesHtml);
+//});
+
 
 app.get("/myForm", (req, res) => res.render("pages/myForm", {qs: req.query}));
 
@@ -22,7 +26,12 @@ app.post("/myForm", (req, res) => {
   // Add your implementation here
   let formData = req.body;
   let movie = formData.movieName;
-  myMovies = movielist.append(movie.replace(/,/g, "\n" , movie[0].toUpperCase() + movie.toLowerCase()));
+
+  //let myhtml = " ";
+  //movieNames.forEach(name => myhtml += "<h1>" + name + "</h1>")
+  //app.get("/hello", (req, res) => res.send(myhtml));
+  movielistdatabase.append(movie.replace(/,/g, "\n" , movie[0].toUpperCase() + movie.toLowerCase()));
+  
   //let movieList = replaceComma(movie);
  // function replaceComma(data) {
     //let dataToarray = data.split(",").map(item => item.trim());
@@ -51,6 +60,10 @@ app.get("/myListQueryString", (req, res) => {
 app.get("/search/:movieName", (req, res) => {
   // Add your implementation here
   let movie = req.query.movieName;
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 🚀");
 });
 
 app.listen(3000, () => {
